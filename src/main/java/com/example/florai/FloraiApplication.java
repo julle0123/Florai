f7cc21211ab9cb6323acd7d0859b928cc824fa92
julle0123 <julle03@naver.com>
2025-04-01 20:55:15 +0900

    세번째 commit

--- src/main/java/com/example/florai/FloraiApplication.java
+++ src/main/java/com/example/florai/FloraiApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FloraiApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {SpringApplication.run(FloraiApplication.class, args);
 
-		SpringApplication.run(FloraiApplication.class, args);
 	}
 
 }
