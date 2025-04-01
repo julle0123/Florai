00a37e4fff732408a1ae813f20f21d34f0e51cdd
julle0123 <julle03@naver.com>
2025-04-01 18:19:43 +0900

    두번째 commit

---
+++ src/main/java/com/example/florai/controller/RegisterController.java
@@ -0,0 +1,8 @@
+package com.example.florai.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RegisterController {
+
+}
---
+++ src/main/java/com/example/florai/entity/User.java
@@ -0,0 +1,16 @@
+package com.example.florai.entity;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+//@Entity // JPA가 관리하는 엔티티 클래스
+//@Data
+//@Getter // Lombok → getter 자동 생성
+//@Setter // Lombok → setter 자동 생성
+//@NoArgsConstructor // Lombok → 기본 생성자 자동 생성
+//@AllArgsConstructor // Lombok → 모든 필드 포함 생성자 자동 생성
+//@ToString
+//public class User {
+//    // Member table 에 들어갈 columns
+//
+//}
