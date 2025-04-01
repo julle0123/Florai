28e08151b94b2f11ea73ee79a6a6a7e56660e8c0
julle0123 <julle03@naver.com>
2025-04-01 09:22:43 +0900

    first commit

---
+++ src/test/java/com/example/florai/FloraiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.florai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloraiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
